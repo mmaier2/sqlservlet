0e80cd865267c89497a0419a21ca0737970f7236
markus <markus@reserv-15-vpn-fw.net.detemobil.de>
2015-03-23 13:38:03 +0100

    adding scripts

---
+++ scripts/create_table.sql
@@ -0,0 +1,11 @@
+create table telefon (
+Name	varchar2(50),
+Vorname	varchar2(50),
+Vorwahl	varchar2(10),
+telnr	varchar2(20));
+
+insert into telefon values ('Obama','Barack','++1 202','555-3658');
+insert into telefon values ('Putin','Vladimir','++7 495','777-3562');
+insert into telefon values ('Merkel','Angela','+49 30','333-4711');
+
+commit;
